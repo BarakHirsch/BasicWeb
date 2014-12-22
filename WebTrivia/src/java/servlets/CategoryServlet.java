0f4a2fdc0682a21a48da3a8bd0d110b9911ad952
baviran <baviran@gmail.com>
2014-12-22 18:59:40 +0200

    Aternative Trivia Home page.
    Continue working with servlets.

--- WebTrivia/src/java/servlets/CategoryServlet.java
+++ WebTrivia/src/java/servlets/CategoryServlet.java
@@ -16,24 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CategoryServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -62,11 +45,11 @@ public class CategoryServlet extends HttpServlet {
                 out.println("<img src=\"gameCategory/images/"+ cat +".jpg\" alt=\"\" title=\"\" class=\"left_img\">");
                 out.println("<br>");
                 out.println("Please choose difficulty for your questions:<br>");
-                out.println("<input type=\"radio\" name=\"radioButton1\" value=\"Easy\" checked> Easy<br><br>");
-                out.println("<input type=\"radio\" name=\"radioButton1\" value=\"Medium\" > Medium<br><br>");
-                out.println("<input type=\"radio\" name=\"radioButton1\" value=\"Hard\"> Hard<br><br>");
+                out.println("<input type=\"radio\" name=\"radioButton"+cat+ "\" value=\"Easy\" checked> Easy<br><br>");
+                out.println("<input type=\"radio\" name=\"radioButton"+cat+"\" value=\"Medium\" > Medium<br><br>");
+                out.println("<input type=\"radio\" name=\"radioButton"+cat+"\" value=\"Hard\"> Hard<br><br>");
                 out.println("<div align=\"center\">");
-                out.println("<input type=\"checkbox\" name=\"GameCategory1\" value=\"GameCategory1\">Check if you like to play in this category<br>");
+                out.println("<input type=\"checkbox\" name=\"checkBox"+cat+"\" value=\""+cat+"Check\">Check if you like to play in this category<br>");
                 out.println("</div>");
                 out.println("</div>");
                 out.println("<hr>");
@@ -79,36 +62,19 @@ public class CategoryServlet extends HttpServlet {
             out.println("</body>");
             out.println("</html>");
 
-            for (Category cat : Category.values()) {
-
-                if (cat.name().equalsIgnoreCase("None")) {
-                    continue;
-                }
-
-            }
+            
 
         }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+    
     @Override
     public String getServletInfo() {
         return "Short description";
--- WebTrivia/src/java/servlets/ProcessGameServlet.java
+++ WebTrivia/src/java/servlets/ProcessGameServlet.java
@@ -11,73 +11,89 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import enums.*;
+import java.util.Enumeration;
 
-/**
- *
- * @author Barda
- */
 public class ProcessGameServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. jnjnj */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProcessGameServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProcessGameServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        int cnt=0;
+        
+        for (Category cat : Category.values()) 
+        {
+                if (cat.name().equalsIgnoreCase("None")) 
+                    continue;
+                if (request.getParameter("checkBox"+cat)!=null)
+                    cnt++;
+        }
+        cnt++;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String docType = "<!DOCTYPE HTML>\n";
+            String title = "Reading All Request Parameters";
+            out.println("<!DOCTYPE HTML><html><head><title>" + title + "</title></head>");
+            out.println("<BODY BGCOLOR=\"#FDF5E6\">\n");
+            out.println("<H1 ALIGN=CENTER>" + title + "</H1>\n");
+            out.println("<TABLE BORDER=1 ALIGN=CENTER>\n");
+            out.println("<TR BGCOLOR=\"#FFAD00\">\n");
+            out.println("<TH>Parameter Name<TH>Parameter Value(s)");
+
+            Enumeration paramNames = request.getParameterNames();
+
+            while (paramNames.hasMoreElements()) {
+                String paramName = (String) paramNames.nextElement();
+                out.print("<TR><TD>" + paramName + "\n<TD>");
+                String[] paramValues = request.getParameterValues(paramName);
+                if (paramValues.length == 1) {
+                    String paramValue = paramValues[0];
+                    if (paramValue.length() == 0) {
+                        out.println("<I>No Value</I>");
+                    } else {
+                        out.println(paramValue);
+                    }
+                } else {
+                    out.println("<UL>");
+                    for (int i = 0; i < paramValues.length; i++) {
+                        out.println("<LI>" + paramValues[i]);
+                    }
+                    out.println("</UL>");
+                }
+            }
+            out.println("</TABLE>\n</BODY></HTML>");
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+        request.getParameter("radioButtonGeneral");
+                */
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
