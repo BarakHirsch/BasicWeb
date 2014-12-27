ef94f17524ebc7687334741f77b8d616d1d7e13d
baviran <baviran@gmail.com>
2014-12-27 19:08:13 +0200

    Revert "Add Question feature. almost done."
    This reverts commit 9319a41badfa62002f7cd2d815786386a05cdae8.

---
+++ Trivia/src/java/servlets/LoginServlet.java
@@ -0,0 +1,81 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        boolean newbie = true;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if ((c.getName().equals("repeatVisitor")) && (c.getValue().equals("yes"))) {
+                    newbie = false;
+                    break;
+                }
+            }
+        }
+        String title;
+        if (newbie) {
+            Cookie returnVisitorCookie = new Cookie("repeatVisitor", "yes");
+            returnVisitorCookie.setMaxAge(60 * 60 * 24 ); // 1 year
+            response.addCookie(returnVisitorCookie);
+            title = "Welcome Aboard";
+        } else {
+            title = "Welcome Back";
+        }
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>" + title + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
