4cfd26b2f54e987d4f7347486d86c546a031fa1d
baviran <baviran@gmail.com>
2014-12-29 13:40:20 +0200

    Login + Logout option added.

--- Trivia/src/java/servlets/UserInfoServlet.java
+++ Trivia/src/java/servlets/UserInfoServlet.java
@@ -44,10 +44,10 @@ public class UserInfoServlet extends HttpServlet {
 
             if (userName == null) {
                 out.print("Hello, Guest");
-                out.println("<a href=\"LoginServlet\" target=\"_parent\">(Login)</a>");
+                out.println("<a href=\"LoginServlet\" target=\"_self\">(Login)</a>");
             } else {
                 out.print("Hello, " + userName);
-                out.println("<a href=\"LogoutServlet\" target=\"_parent\">(Logout)</a>");
+                out.println("<a href=\"LogoutServlet\" target=\"_self\">(Logout)</a>");
             }
             out.print("</h3>");
             out.println("</body>");
@@ -59,69 +59,39 @@ public class UserInfoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Cookie[] cookies = request.getCookies();
-        String firstName = "";
-        String lastName = "";
+
+        Cookie userNameCookie = new Cookie("UserName", request.getParameter("firstName") +" "+ request.getParameter("lastName"));
+        userNameCookie.setMaxAge(60 * 60 * 24);
+        response.addCookie(userNameCookie);
+       
+        String userName = userNameCookie.getValue();
+
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");
             out.println("<style>");
-            out.println("#loginSection{background-color:black;color:white;text-align:left;font: 10px  verdana, arial, helvetica;}");
-            out.println("#header {background-color:black;color:white;text-align:center;padding:5px;font: 30px  verdana, arial, helvetica;}");
-            out.println("#nav {line-height:30px;background-color:#eeeeee;height:500px;width:100px;float:left;padding:5px;font: 12px  verdana, arial, helvetica;}");
-            out.println("#section {width:350px;float:left;padding:10px;font: 20px  verdana, arial, helvetica;}");
-            out.println("#footer {background-color:black;color:white;clear:both;text-align:center;padding:5px;font: 20px  verdana, arial, helvetica;}");
+            out.println("h3 {color:white;font: 14px  verdana, arial, helvetica;}");
             out.println("</style>");
             out.println("</head>");
             out.println("<body>");
-            if (cookies != null) {
-                for (Cookie c : cookies) {
-                    if ((c.getName().equals("firstName"))) {
-                        firstName = c.getValue();
-                    }
-                    if ((c.getName().equals("lastName"))) {
-                        lastName = c.getValue();
-                    }
-                }
-                if (!("".equals(firstName))) {
-                    out.println("<div id=\"loginSection\">");
-                    out.println("<br>");
-                    out.println("Welcome Back, " + firstName + " " + lastName);
-                    out.println("");
-                    out.println("</div>");
-                }
+
+            out.print("<h3>");
+
+            if (userName == null) {
+                out.print("Hello, Guest");
+                out.println("<a href=\"LoginServlet\" target=\"_self\">(Login)</a>");
+            } else {
+                out.print("Hello, " + userName);
+                out.println("<a href=\"LogoutServlet\" target=\"_self\">(Logout)</a>");
             }
-
-            if ("".equals(firstName)) {
-                Cookie userNameCookie = new Cookie("UserName", request.getParameter("firstName") + request.getParameter("lastName"));
-
-                userNameCookie.setMaxAge(60 * 60 * 24);
-
-                response.addCookie(userNameCookie);
-            }
-            out.println("<div id=\"header\">");
-            out.println("<h1>Trivia</h1>");
-            out.println("</div>");
-            out.println("<div id=\"nav\">");
-            out.println("<a href=\"generalInfo.html\" target=\"sectionFrame\">Information</a><br><br>");
-            out.println("<a href=\"AnotherTrivia.html\" target=\"sectionFrame\">Links</a><br><br>");
-            out.println("<a href=\"StartGameServlet\" target=\"sectionFrame\">Game start</a><br><br>");
-            out.println("<a href=\"addQuestion.html\" target=\"sectionFrame\">Add a Question</a><br><br>");
-            out.println("</div>");
-            out.println("<div id=\"section\">");
-            out.println("<iframe frameborder=\"0\" name=\"sectionFrame\" width=\"1000\" height=\"500\">");
-            out.println("</iframe>");
-            out.println("</div>");
-            out.println("<div id=\"footer\">");
-            out.println("<p>Copyright  ©  Barak & Aviran</p>");
-            out.println("</div>");
+            out.print("</h3>");
             out.println("</body>");
             out.println("</html>");
 
         }
+      
     }
 
     @Override
