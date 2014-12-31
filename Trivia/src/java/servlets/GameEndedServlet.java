79090c6ccf39b01d1598269111d65524bee160b4
baviran <aviran87>
2014-12-31 10:22:59 +0200

    Some cosmetic changes

--- Trivia/src/java/servlets/GameEndedServlet.java
+++ Trivia/src/java/servlets/GameEndedServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,6 +32,8 @@ public class GameEndedServlet extends HttpServlet {
             out.println("<style>");
             out.println("h1 {color:black;font: 40px  verdana, arial, helvetica;}");
             out.println("h3 {color:black;font: 20px  verdana, arial, helvetica;}");
+            out.println("h4 {color:black;font: 18px  verdana, arial, helvetica;}");
+            out.println("h5 {color:red;font: 18px  verdana, arial, helvetica;}");
             out.println("</style>");
             out.println("</head>");
             out.println("<body>");
@@ -40,7 +43,13 @@ public class GameEndedServlet extends HttpServlet {
             for (Map.Entry<Category, Integer> answerCount : currentGame.getAnswersCount().entrySet()) {
                 out.println("<h3>In " + answerCount.getKey()+ " category, you answerd " + answerCount.getValue()+ " questions correctly.</h3>");
             }
-
+            
+            Cookie[] cookies = request.getCookies();            
+                for (Cookie c : cookies) {
+                    if ((c.getName().equals("UserName"))) {
+                       out.println("<h4>Thank you for playing, <h5>"+ c.getValue() + "</h5></h4>");  ;
+                    }
+                }                                    
             out.println("</body>");
             out.println("</html>");
 
